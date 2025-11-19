6752dd1a55525421009ebae50190ae8d10f728ed
luyuan <luyuan202509@gmail.com>
2025-11-19 21:07:32 +0800

    修改训练过程

--- ch01/train_custom_loop.py
+++ ch01/train_custom_loop.py
@@ -9,10 +9,12 @@ from common.optimizer import SGD
 import time
 
 # 设置超参数
-max_epoch = 1000
+max_epoch = 300
 batch_size = 30
-hidden_size = 30 
-learning_rate = 0.9
+hidden_size = 10 
+learning_rate = 1.0
+
+
 
 #读入数据,生成模型和优化器
 x, t = spiral.load_data()
@@ -29,29 +31,30 @@ start_time = time.time()
 
 for epoch in range(max_epoch):
     # 打乱数据
-    idx =np.random.permutation(data_size)
+    idx = np.random.permutation(data_size)
     x = x[idx]
     t = t[idx]
+
     for iters in range(max_iters):
         batch_x = x[iters*batch_size:(iters+1)*batch_size]
         batch_t = t[iters*batch_size:(iters+1)*batch_size]
 
-    
-    # 计算梯度，更新参数
-    loss = model.forward(batch_x,batch_t)
-    model.backward()
-    optimizer.update(model.params,model.grads)
-    total_loss += loss
-    loss_count += 1
-    
-    # 评价 定期输出学习过程
-    if (iters+1) % 10 == 0:
-        avg_loss = total_loss / loss_count
-        print(f"Epoch {epoch+1} / {max_epoch}, Iter {iters+1} / {max_iters}, Loss {avg_loss}")
-        loss_list.append(
-            
-        )
-        total_loss, loss_count = 0, 0
+        # 计算梯度，更新参数
+        loss = model.forward(batch_x, batch_t)
+        model.backward()
+        optimizer.update(model.params, model.grads)
+
+        total_loss += loss
+        loss_count += 1
+
+        # 定期输出学习过程
+        if (iters+1) % 10 == 0:
+            avg_loss = total_loss / loss_count
+            print('| epoch %d |  iter %d / %d | loss %.2f' % (epoch + 1, iters + 1, max_iters, avg_loss))
+            loss_list.append(avg_loss)
+            total_loss, loss_count = 0, 0
+
+
 
 elapsed_time = time.time() - start_time
 print(f"训练时间： {elapsed_time}[s]")
